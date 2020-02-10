1651d4dae49c270b0f2f9228c73331236eafa6f8
Desmond <edemintech@gmail.com>
2020-02-10 13:23:22 +0100

    feat(comment): add comment to ticket (#4)

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,16 +8,24 @@ const router = Router();
 
 const {
   createTicket,
+  createComment,
 } = ticketController;
 
 const {
   createTicketSchema,
+  createCommentSchema,
 } = ticketSchema;
 
 router.post(
   '/tickets',
   validator(createTicketSchema),
   asyncWrapper(createTicket),
+);
+
+router.post(
+  '/tickets/:ticketId/comment',
+  validator(createCommentSchema),
+  asyncWrapper(createComment),
 );
 
 export default router;
