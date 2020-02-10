97b1c88d8a39a640c51d418fbbbd5ef048f33b2c
Desmond <edemintech@gmail.com>
2020-02-10 11:39:36 +0100

    chore(setup): setup project directory (#1)

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import compression from 'compression';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+
+dotenv.config();
+
+const app = express();
+
+app.use(compression());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+if (['development', 'staging', 'production'].includes(process.env.NODE_ENV)) {
+  app.use(morgan('dev'));
+}
+
+app.get('/', (_, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'welcome to Ticket',
+  });
+});
+
+app.all('*', (_, res) => {
+  res.status(404).json({
+    status: 'error',
+    error: 'resource not found',
+  });
+});
+
+export default app;
